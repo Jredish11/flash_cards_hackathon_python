19d1f7f4cc59db50e77a116607681dcc216babc0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-08 14:35:26 -0600

    add tests for Turn class objects and functions

---
+++ test_turn.py
@@ -0,0 +1,34 @@
+import pdb
+from card import Card
+from turn import Turn
+
+def test_turn_exists():
+    card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+    turn = Turn("Juneau", card)
+
+    assert isinstance(turn, Turn)
+
+def test_turn_has_card():
+    card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+    turn = Turn("Juneau", card)
+
+    assert turn.card == card
+
+def test_turn_has_guess():
+    card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+    turn = Turn("Juneau", card)
+
+    assert turn.guess == "Juneau"
+
+def test_correct():
+  card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+  turn = Turn("Juneau", card)
+
+
+  assert turn.correct() == True
+
+def test_feedback():
+  card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+  turn = Turn("Juneau", card)
+
+  assert turn.feedback() == "Correct!"
